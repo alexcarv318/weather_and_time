cc11aaa81af63cd38ce5d2b420a79c0827b611b9
Alexandre Ilchenko <130691110+alexcarv318@users.noreply.github.com>
2023-07-17 19:05:42 +0100

    Update settings.py

--- weather_and_time/settings.py
+++ weather_and_time/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tb=pk52mi=7+53q-vk(&r3bcr2pj&bwl&&lv1)b3@q7#!4xt6-'
+SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
